77561b7b51342d9ba23239c4ee525e27a5b0f304
zahra riahi <riahizahra67@gmail.com>
2024-01-12 17:05:22 +0330

    session 1 committed

---
+++ Session 1-MVC Structure/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Session_1.Models;
+
+namespace Sample;
+
+public class HomeController : Controller
+{
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
---
+++ Session 1-MVC Structure/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Session_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
